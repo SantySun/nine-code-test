84b91d4e87929a456adeb870af8f46c822616bf6
Jixiang Sun <sunjixiang.gz@gmail.com>
2021-04-22 11:18:12 +1000

    define header

--- server.js
+++ server.js
@@ -3,29 +3,40 @@ const cors = require("cors")
 const bodyparser = require("body-parser")
 const port = process.env.PORT || 5000
 const app = express()
-app.use(cors());
+app.use(cors())
+
+app.use(bodyparser.json())
 
 app.use(function (err, req, res, next) {
   console.error(err.stack)
-  res.status(400).json({ error: "Could not decode request: JSON parsing failed" })
+  res.setHeader("content-type", "application/json")
+  return res
+    .status(400)
+    .json({ error: "Could not decode request: JSON parsing failed" })
 })
-app.use(bodyparser.json())
 
 app.post("/", (req, res) => {
   try {
     const { payload } = req.body
-    const result = payload.map((show) => {
-      if (show.drm && show.episodeCount) {
-        return {
-          image: show.image.showImage,
-          slug: show.slug,
-          title: show.title
+    const result = payload
+      .map((show) => {
+        if (show.drm && show.episodeCount) {
+          return {
+            image: show.image.showImage,
+            slug: show.slug,
+            title: show.title
+          }
         }
-      }
-    }).filter((show) => !!show)
+      })
+      .filter((show) => !!show)
+
+    res.setHeader("content-type", "application/json")
     return res.status(200).json({ response: result })
   } catch (error) {
-    return res.status(400).json({ error: "Could not decode request: JSON parsing failed" })
+    res.setHeader("content-type", "application/json")
+    return res
+      .status(400)
+      .json({ error: "Could not decode request: JSON parsing failed" })
   }
 })
 
